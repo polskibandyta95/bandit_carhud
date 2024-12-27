3dfbddcbff3a89bb4f2719b810117601be0d83a3
polskibandyta95 <marekgolecpraca@gmail.com>
2024-12-27 13:09:50 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,9 @@
 local isDriver = false
-local barsTotal = 16 -- Total number of bars for RPM meter
-local barsActive = 0 -- This will be calculated based on RPM
+local barsTotal = 16 
+local barsActive = 0
 
--- Function to update the active bars based on RPM
 function UpdateRPMBar(rpm)
-    local maxRpm = 1.0 -- Maximum RPM, usually 1.0 for most vehicles
+    local maxRpm = 1.0
     barsActive = math.floor((rpm / maxRpm) * barsTotal)
 end
 
@@ -14,19 +13,16 @@ Citizen.CreateThread(function()
         local vehicle = GetVehiclePedIsIn(playerPed, false)
 
         if vehicle ~= 0 and DoesEntityExist(vehicle) then
-            -- Player is inside a valid vehicle
-            local speed = GetEntitySpeed(vehicle) * 3.6 -- Convert m/s to km/h
+            local speed = GetEntitySpeed(vehicle) * 3.6 
             local rpm = GetVehicleCurrentRpm(vehicle)
             local gear = GetVehicleCurrentGear(vehicle)
             local fuel = GetVehicleFuelLevel(vehicle)
             local engineHealth = GetVehicleEngineHealth(vehicle)
             local doorsOpen = false
-            local seatbeltOn = IsPedWearingHelmet(playerPed) -- Assuming helmet is used as seatbelt indicator
+            local seatbeltOn = IsPedWearingHelmet(playerPed)
 
-            -- Update the RPM progress bar
             UpdateRPMBar(rpm)
 
-            -- Check if any door is open
             for i = 0, GetNumberOfVehicleDoors(vehicle) - 1 do
                 if IsVehicleDoorDamaged(vehicle, i) then
                     doorsOpen = true
@@ -34,7 +30,6 @@ Citizen.CreateThread(function()
                 end
             end
 
-            -- Ensure all players in the vehicle see the speedometer
             SendNUIMessage({
                 type = "updateSpeedometer",
                 speed = speed,
@@ -45,11 +40,10 @@ Citizen.CreateThread(function()
                 engineHealth = engineHealth,
                 doorsOpen = doorsOpen,
                 seatbeltOn = seatbeltOn,
-                isDriver = GetPedInVehicleSeat(vehicle, -1) == playerPed, -- Indicate if the player is the driver
+                isDriver = GetPedInVehicleSeat(vehicle, -1) == playerPed, 
                 barsActive = barsActive
             })
         else
-            -- Player is not in a vehicle, hide the speedometer
             SendNUIMessage({
                 type = "hideSpeedometer"
             })
