12d7b5fe5371b34ed6341114278305858a0f5b04
polskibandyta95 <marekgolecpraca@gmail.com>
2024-12-27 13:08:57 +0000

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,7 @@
 shared_script '@virtual_custombronie/shared_fg-obfuscated.lua'
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'Virtual Speedo'
+description 'Car Hud by PolishBandit'
 
 client_script 'client.lua'
 
